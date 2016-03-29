3d4216e663710d03c26e3d3784b8d8f2c34edc22
Nuno Picado <tecnica@acesso.com.pt>
2016-03-29 11:52:33 +0100

    Added a generic interface to use in component conversion to interfaced variant

---
+++ Obj.SSI.GenericIntf.pas
@@ -0,0 +1,12 @@
+unit Obj.SSI.GenericIntf;
+
+interface
+
+type
+    I<T> = Interface                ['{D6A7F28C-56D5-4D59-8C58-41489D9A3098}']
+      function Obj: T;
+    End;
+	
+implementation
+
+end.
