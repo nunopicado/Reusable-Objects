376570c00ec39f8fadbaf8fc1e4a3fbedfceaa8b
Nuno Picado <tecnica@acesso.com.pt>
2016-03-29 11:53:16 +0100

    Created interfaced version of RTL.TStringList

---
+++ Obj.RTL.IStringList.pas
@@ -0,0 +1,44 @@
+unit Obj.RTL.IStringList;
+
+interface
+
+uses
+    Classes, Obj.SSI.GenericIntf;
+
+type
+    TIStringList = Class(TInterfacedObject, I<TStringList>)
+    private
+      FObj: TStringList;
+    public
+      constructor Create;
+      destructor Destroy; Override;
+      class function New: I<TStringList>;
+      function Obj: TStringList;
+    End;
+
+implementation
+
+{ TIStringList }
+
+constructor TIStringList.Create;
+begin
+     FObj := TStringList.Create;
+end;
+
+destructor TIStringList.Destroy;
+begin
+     FObj.Free;
+     inherited;
+end;
+
+class function TIStringList.New: I<TStringList>;
+begin
+     Result := Create;
+end;
+
+function TIStringList.Obj: TStringList;
+begin
+     Result := FObj;
+end;
+
+end.
