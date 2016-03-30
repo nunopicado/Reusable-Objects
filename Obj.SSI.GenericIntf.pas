c4af873672b1faa03e3ec2a23a8dee32b02825e1
Nuno Picado <tecnica@acesso.com.pt>
2016-03-30 18:12:46 +0100

    Indentation Fix

--- Obj.SSI.GenericIntf.pas
+++ Obj.SSI.GenericIntf.pas
@@ -3,7 +3,7 @@ unit Obj.SSI.GenericIntf;
 interface
 
 type
-    I<T> = Interface                ['{D6A7F28C-56D5-4D59-8C58-41489D9A3098}']
+    I<T> = Interface                  ['{D6A7F28C-56D5-4D59-8C58-41489D9A3098}']
       function Obj: T;
     End;
 	
