7b4d3d160baeba40a0ec938d2e7009de22a31dca
Nuno Picado <tecnica@acesso.com.pt>
2016-03-31 16:48:35 +0100

    Added a standard exception to be used by all raising requirements in the Reusable Objects project

--- Obj.SSI.GenericIntf.pas
+++ Obj.SSI.GenericIntf.pas
@@ -2,7 +2,12 @@ unit Obj.SSI.GenericIntf;
 
 interface
 
+uses
+    SysUtils;
+
 type
+    ESSIError = Class(Exception);
+
     I<T> = Interface                  ['{D6A7F28C-56D5-4D59-8C58-41489D9A3098}']
       function Obj: T;
     End;
