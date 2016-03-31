15fa3160ae3c5ff5e287f33e933dce6f5c8ebe60
Nuno Picado <tecnica@acesso.com.pt>
2016-03-31 17:23:39 +0100

    [UNI.IUniQuery] Refactored so the constructor's only responsability is to initialize self class plus internally needed objects
    SQL Statement assignment, parameter assignment and execution command are now each in its own method, and not in the constructor
    An overloaded version of the New class method allows to run current SQL statement with no parameters

--- Obj.UNI.IUniQuery.pas
+++ Obj.UNI.IUniQuery.pas
@@ -7,15 +7,22 @@ uses
 
 type
     I<T> = Interface (Obj.SSI.GenericIntf.I<T>) ['{F7928CA1-6E47-400E-9BC4-DEAEB1E72AC6}']
+      function Run(const Params: Array of Variant): I<TUniQuery>; Overload;
+      function Run: I<TUniQuery>; Overload;
+      function SQL(const SQLStatement: String): I<TUniQuery>;
     End;
 
     TIUniQuery = Class(TInterfacedObject, I<TUniQuery>)
     private
       FObj: TUniQuery;
     public
-      constructor Create(aConnection: TUniConnection; SQL: String; Params: Array of Variant); Overload;
-      class function New(aConnection: TUniConnection; SQL: String; Params: Array of Variant): I<TUniQuery>; Overload;
-      class function New(aConnection: TUniConnection; SQL: String): I<TUniQuery>; Overload;
+      constructor Create(aConnection: TUniConnection); Overload;
+      destructor Destroy; Override;
+      class function New(aConnection: TUniConnection; const SQLStatement: String): I<TUniQuery>; Overload;
+      class function New(aConnection: TUniConnection): I<TUniQuery>; Overload;
+      function Run(const Params: Array of Variant): I<TUniQuery>; Overload;
+      function Run: I<TUniQuery>; Overload;
+      function SQL(const SQLStatement: String): I<TUniQuery>;
       function Obj: TUniQuery;
     End;
 
@@ -23,41 +30,31 @@ implementation
 
 { TIUniQuery }
 
-constructor TIUniQuery.Create(aConnection: TUniConnection; SQL: String; Params: Array of Variant);
-var
-   p: Integer;
+constructor TIUniQuery.Create(aConnection: TUniConnection);
 begin
-     if (not Assigned(aConnection)) or (not aConnection.Connected) or (SQL='')
-        then Exit;
+     if (not Assigned(aConnection)) or (not aConnection.Connected)
+        then raise ESSIError.CreateFmt('Cannot access database!%sConnection ''%s'' unavailable or offline.',[#10,aConnection.Name]);
 
      inherited Create;
      FObj                       := TUniQuery.Create(nil);
      FObj.Connection            := aConnection;
      FObj.Options.QueryRecCount := True;
+end;
 
-     if Length(Params)>0
-        then begin
-                  p := 0;
-                  repeat
-                        ParamByName(String(Params[p])).Value := Params[p+1];
-                        Inc(p,2);
-                  until p >= High(Params)-1;
-             end;
-     Self.Open;
 destructor TIUniQuery.Destroy;
 begin
      FObj.Free;
      inherited;
 end;
 
-class function TIUniQuery.New(aConnection: TUniConnection; SQL: String; Params: Array of Variant): I<TUniQuery>;
+class function TIUniQuery.New(aConnection: TUniConnection): I<TUniQuery>;
 begin
-     Result := Create(aConnection, SQL, Params);
+     Result := Create(aConnection);
 end;
 
-class function TIUniQuery.New(aConnection: TUniConnection; SQL: String): I<TUniQuery>;
+class function TIUniQuery.New(aConnection: TUniConnection; const SQLStatement: String): I<TUniQuery>;
 begin
-     Result := Create(aConnection, SQL, []);
+     Result := Create(aConnection).SQL(SQLStatement);
 end;
 
 function TIUniQuery.Obj: TUniQuery;
@@ -65,7 +62,37 @@ begin
      Result := FObj;
 end;
 
+function TIUniQuery.Run: I<TUniQuery>;
+begin
+     Result := Run([]);
+end;
+
+function TIUniQuery.SQL(const SQLStatement: String): I<TUniQuery>;
+begin
      Result := Self;
+
+     if SQLStatement=''
+        then raise ESSIError.Create('Cannot run an empty query!');
+
+     FObj.SQL.Text := SQLStatement;
+end;
+
+function TIUniQuery.Run(const Params: array of Variant): I<TUniQuery>;
+var
+   Param: Integer;
+begin
+     Result := Self;
+
+     if Odd(Length(Params))
+        then raise ESSIError.CreateFmt('Invalid parameter list!%sOdd parameter number in SQL Statement',[#10]);
+
+     Param := 0;
+     while Param < Length(Params) do
+           begin
+                FObj.ParamByName(Params[Param]).Value := Params[Param + 1];
+                Inc(Param, 2);
+           end;
+     FObj.Open;
 end;
 
 end.
