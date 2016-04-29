f0786c30cc3166ed9f098abee9c2a314187f088e
Nuno Picado <tecnica@acesso.com.pt>
2016-04-29 11:22:45 +0100

    [IStringList] New: Created function to add a line to the list

--- Obj.RTL.IStringList.pas
+++ Obj.RTL.IStringList.pas
@@ -6,6 +6,10 @@ uses
     Classes, Obj.SSI.GenericIntf;
 
 type
+    I<T> = Interface (Obj.SSI.GenericIntf.I<T>)           ['{D6A7F28C-56D5-4D59-8C58-41489D9A3098}']
+      function Add(s: String): I<TStringList>;
+    End;
+
     TIStringList = Class(TInterfacedObject, I<TStringList>)
     private
       FObj: TStringList;
@@ -13,12 +17,19 @@ type
       constructor Create(Text: String); Overload;
       destructor Destroy; Override;
       class function New(Text: String): I<TStringList>;
+      function Add(s: String): I<TStringList>;
       function Obj: TStringList;
     End;
 
 implementation
 
 { TIStringList }
+
+function TIStringList.Add(s: String): I<TStringList>;
+begin
+     Result := Self;
+     FObj.Add(s);
+end;
 
 constructor TIStringList.Create(Text: String);
 begin
