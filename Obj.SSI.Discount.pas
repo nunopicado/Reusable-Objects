000fcc2567015724bbe29126cb29e3711ff2475f
Nuno Picado <tecnica@acesso.com.pt>
2016-04-30 23:41:14 +0100

    [Reusable Objects] New: Added information panel to each unit

--- Obj.SSI.Discount.pas
+++ Obj.SSI.Discount.pas
@@ -1,3 +1,28 @@
+(******************************************************************************)
+(** Suite         : Reusable Objects                                         **)   
+(** Object        : IDiscount                                                **)
+(** Framework     :                                                          **)
+(** Developed by  : Nuno Picado                                              **)
+(******************************************************************************)
+(** Interfaces    : IDiscount                                                **)
+(** Classes       : TDiscount, implements IDiscount                          **)
+(******************************************************************************)
+(** Dependencies  : ICurrency                                                **)
+(**               : RTL                                                      **)
+(******************************************************************************)
+(** Description   : Applies discount actions to ICurrency values             **)
+(******************************************************************************)
+(** Licence       : GNU LGPLv3 (http://www.gnu.org/licenses/lgpl-3.0.html)   **)
+(** Contributions : You can create pull request for all your desired         **)
+(**                 contributions as long as they comply with the guidelines **)
+(**                 you can find in the readme.md file in the main directory **)
+(**                 of the Reusable Objects repository                       **)
+(** Disclaimer    : The licence agreement applies to the code in this unit   **)
+(**                 and not to any of its dependencies, which have their own **)
+(**                 licence agreement and to which you must comply in their  **)
+(**					terms													 **)
+(******************************************************************************)
+
 unit Obj.SSI.Discount;
 
 interface
