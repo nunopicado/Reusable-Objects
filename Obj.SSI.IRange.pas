16f85bf56b263bbb1fbc24c42eb301b6c5e89450
Nuno Picado <geral@nunopicado.com>
2017-01-22 14:35:42 +0000

    [IRange] New: Created a series of objects that represent a range and allows to check if a value is in or out of the range.
    Contituted by IIntegerRange (Int64), IFloatRange (Extended) and ICharRange (Char), can be used standalone (TIntegerRange, TFloatRange and TCharRange) or directly by an aggregated implementation called TRange.

---
+++ Obj.SSI.IRange.pas
@@ -0,0 +1,264 @@
+unit Obj.SSI.IRange;
+
+interface
+
+type
+    IIntegerRange = Interface ['{A9A63E0E-5B8F-464F-851F-A696A95F2A37}']
+      function Includes(const Value: Int64): Boolean;
+      function Excludes(const Value: Int64): Boolean;
+    End;
+
+    IFloatRange = Interface ['{A66B3186-43C2-4F43-840F-5EDD727C2E5F}']
+      function Includes(const Value: Extended): Boolean;
+      function Excludes(const Value: Extended): Boolean;
+    End;
+
+    ICharRange = Interface ['{7E05FC00-9637-41B0-9B90-3A37CB54EC39}']
+      function Includes(const Value: Char): Boolean;
+      function Excludes(const Value: Char): Boolean;
+    End;
+
+    TAggregatedIntegerRange = Class(TAggregatedObject, IIntegerRange)
+    strict private
+      FOrigin: IIntegerRange;
+    public
+      constructor Create(Origin: IIntegerRange);
+      function Includes(const Value: Int64): Boolean;
+      function Excludes(const Value: Int64): Boolean;
+    End;
+
+    TIntegerRange = Class(TInterfacedObject, IIntegerRange)
+    strict private
+      FLow  : Int64;
+      FHigh : Int64;
+    public
+      constructor Create(const LowValue, HighValue: Int64);
+      class function New(const LowValue, HighValue: Int64): IIntegerRange;
+      function Includes(const Value: Int64): Boolean;
+      function Excludes(const Value: Int64): Boolean;
+    End;
+
+    TAggregatedFloatRange = Class(TAggregatedObject, IFloatRange)
+    strict private
+      FOrigin: IFloatRange;
+    public
+      constructor Create(Origin: IFloatRange);
+      function Includes(const Value: Extended): Boolean;
+      function Excludes(const Value: Extended): Boolean;
+    End;
+
+    TFloatRange = Class(TInterfacedObject, IFloatRange)
+    strict private
+      FLow  : Extended;
+      FHigh : Extended;
+    public
+      constructor Create(const LowValue, HighValue: Extended);
+      class function New(const LowValue, HighValue: Extended): IFloatRange;
+      function Includes(const Value: Extended): Boolean;
+      function Excludes(const Value: Extended): Boolean;
+    End;
+
+    TAggregatedCharRange = Class(TAggregatedObject, ICharRange)
+    strict private
+      FOrigin: ICharRange;
+    public
+      constructor Create(Origin: ICharRange);
+      function Includes(const Value: Char): Boolean;
+      function Excludes(const Value: Char): Boolean;
+    End;
+
+    TCharRange = Class(TInterfacedObject, ICharRange)
+    strict private
+      FLow  : Char;
+      FHigh : Char;
+    public
+      constructor Create(const LowValue, HighValue: Char);
+      class function New(const LowValue, HighValue: Char): ICharRange;
+      function Includes(const Value: Char): Boolean;
+      function Excludes(const Value: Char): Boolean;
+    End;
+
+    TRange = Class(TInterfacedObject, IIntegerRange, IFloatRange, ICharRange)
+    strict private
+      FIntegerRange : TAggregatedIntegerRange;
+      FFloatRange   : TAggregatedFloatRange;
+      FCharRange    : TAggregatedCharRange;
+      constructor Create(const LowValue, HighValue: Int64); Overload;
+      constructor Create(const LowValue, HighValue: Extended); Overload;
+      constructor Create(const LowValue, HighValue: Char); Overload;
+    public
+      class function New(const LowValue, HighValue: Int64)    : IIntegerRange; Overload;
+      class function New(const LowValue, HighValue: Extended) : IFloatRange; Overload;
+      class function New(const LowValue, HighValue: Char)     : ICharRange; Overload;
+      destructor Destroy; Override;
+      property IntegerRange : TAggregatedIntegerRange read FIntegerRange implements IIntegerRange;
+      property FloatRange   : TAggregatedFloatRange   read FFloatRange   implements IFloatRange;
+      property CharRange    : TAggregatedCharRange    read FCharRange    implements ICharRange;
+    End;
+
+implementation
+
+{ TIntegerRange }
+
+constructor TIntegerRange.Create(const LowValue, HighValue: Int64);
+begin
+     FLow  := LowValue;
+     FHigh := HighValue;
+end;
+
+function TIntegerRange.Excludes(const Value: Int64): Boolean;
+begin
+     Result := not Includes(Value);
+end;
+
+function TIntegerRange.Includes(const Value: Int64): Boolean;
+begin
+     Result := (Value >= FLow) and (Value <= FHigh);
+end;
+
+class function TIntegerRange.New(const LowValue, HighValue: Int64): IIntegerRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+{ TFloatRange }
+
+constructor TFloatRange.Create(const LowValue, HighValue: Extended);
+begin
+     FLow  := LowValue;
+     FHigh := HighValue;
+end;
+
+function TFloatRange.Excludes(const Value: Extended): Boolean;
+begin
+     Result := not Includes(Value);
+end;
+
+function TFloatRange.Includes(const Value: Extended): Boolean;
+begin
+     Result := (Value >= FLow) and (Value <= FHigh);
+end;
+
+class function TFloatRange.New(const LowValue, HighValue: Extended): IFloatRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+{ TCharRange }
+
+constructor TCharRange.Create(const LowValue, HighValue: Char);
+begin
+     FLow  := LowValue;
+     FHigh := HighValue;
+end;
+
+function TCharRange.Excludes(const Value: Char): Boolean;
+begin
+     Result := not Includes(Value);
+end;
+
+function TCharRange.Includes(const Value: Char): Boolean;
+begin
+     Result := (Value >= FLow) and (Value <= FHigh);
+end;
+
+class function TCharRange.New(const LowValue, HighValue: Char): ICharRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+{ TRange }
+
+constructor TRange.Create(const LowValue, HighValue: Int64);
+begin
+     FIntegerRange := TAggregatedIntegerRange.Create(TIntegerRange.Create(LowValue, HighValue));
+end;
+
+constructor TRange.Create(const LowValue, HighValue: Extended);
+begin
+     FFloatRange := TAggregatedFloatRange.Create(TFloatRange.Create(LowValue, HighValue));
+end;
+
+constructor TRange.Create(const LowValue, HighValue: Char);
+begin
+     FCharRange := TAggregatedCharRange.Create(TCharRange.Create(LowValue, HighValue));
+end;
+
+destructor TRange.Destroy;
+begin
+     if Assigned(FIntegerRange)
+        then FIntegerRange.Free;
+     if Assigned(FFloatRange)
+        then FFloatRange.Free;
+     if Assigned(FCharRange)
+        then FCharRange.Free;
+     inherited;
+end;
+
+class function TRange.New(const LowValue, HighValue: Int64): IIntegerRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+class function TRange.New(const LowValue, HighValue: Extended): IFloatRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+class function TRange.New(const LowValue, HighValue: Char): ICharRange;
+begin
+     Result := Create(LowValue, HighValue);
+end;
+
+{ TDecorableIntegerRange }
+
+constructor TAggregatedIntegerRange.Create(Origin: IIntegerRange);
+begin
+     FOrigin := Origin;
+end;
+
+function TAggregatedIntegerRange.Excludes(const Value: Int64): Boolean;
+begin
+     Result := FOrigin.Excludes(Value);
+end;
+
+function TAggregatedIntegerRange.Includes(const Value: Int64): Boolean;
+begin
+     Result := FOrigin.Includes(Value);
+end;
+
+{ TDecorableFloatRange }
+
+constructor TAggregatedFloatRange.Create(Origin: IFloatRange);
+begin
+     FOrigin := Origin;
+end;
+
+function TAggregatedFloatRange.Excludes(const Value: Extended): Boolean;
+begin
+     Result := FOrigin.Excludes(Value);
+end;
+
+function TAggregatedFloatRange.Includes(const Value: Extended): Boolean;
+begin
+     Result := FOrigin.Includes(Value);
+end;
+
+{ TDecorableCharRange }
+
+constructor TAggregatedCharRange.Create(Origin: ICharRange);
+begin
+     FOrigin := Origin;
+end;
+
+function TAggregatedCharRange.Excludes(const Value: Char): Boolean;
+begin
+     Result := FOrigin.Excludes(Value);
+end;
+
+function TAggregatedCharRange.Includes(const Value: Char): Boolean;
+begin
+     Result := FOrigin.Includes(Value);
+end;
+
+end.
