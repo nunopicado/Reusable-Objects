00ee56ed38bc8aa571f1ec72b853a628e2ef1d61
Nuno Picado <geral@nunopicado.com>
2017-10-25 18:27:35 +0100

    Added unit test for TIf class

---
+++ test/uTIfTest.pas
@@ -0,0 +1,39 @@
+unit uTIfTest;
+
+interface
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TIfTest = class(TObject)
+  public
+    [Test]
+    [TestCase('True 1','True,1,1,1')]
+    [TestCase('False 1','False,1,2,2')]
+    procedure EvalTest(const Condition: Boolean; const v1, v2, Expected: Integer);
+  end;
+
+implementation
+
+uses
+    Obj.SSI.TIf
+  ;
+
+{ TIfTest }
+
+procedure TIfTest.EvalTest(const Condition: Boolean; const v1, v2, Expected: Integer);
+begin
+  Assert.AreEqual<Integer>(
+    Expected,
+    TIf<Integer>.New(
+      Condition,
+      v1,
+      v2
+    ).Eval
+  );
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TIfTest);
+end.
