a3839fc8b72f621fc3e797d264bfff74a14d6a21
Nuno Picado <geral@nunopicado.com>
2017-10-25 18:42:47 +0100

    Cosmetic

--- src/DB/Obj.SSI.DBGenericImpl.pas
+++ src/DB/Obj.SSI.DBGenericImpl.pas
@@ -34,59 +34,60 @@ uses
   ;
 
 type
-    TServer = class(TInterfacedObject, IServer)
-    private
-      FHostname: String;
-      FPort: Word;
-      FUsername: String;
-      FPassword: String;
-      FServerType: TServerType;
-    public
-      constructor Create(const HostName: String; const Port: Word; const Username, Password: String; ServerType: TServerType);
-      class function New(const HostName: String; const Port: Word; const Username, Password: String; ServerType: TServerType): IServer;
-      function Hostname: String;
-      function Port: Word;
-      function Username: String;
-      function Password: String;
-      function ServerType: TServerType;
-      function TypeAsString: String;
-    End;
+  TServer = class(TInterfacedObject, IServer)
+  private
+    FHostname: string;
+    FPort: Word;
+    FUsername: string;
+    FPassword: string;
+    FServerType: TServerType;
+  public
+    constructor Create(const HostName: string; const Port: Word; const Username, Password: string; ServerType: TServerType);
+    class function New(const HostName: string; const Port: Word; const Username, Password: string; ServerType: TServerType): IServer;
+    function Hostname: string;
+    function Port: Word;
+    function Username: string;
+    function Password: string;
+    function ServerType: TServerType;
+    function TypeAsString: string;
+  end;
 
-    TSQLStatement = Class(TInterfacedObject, ISQLStatement)
-    strict private
-      FStatement: String;
-      FParamList: ISQLParams;
-    public
-      constructor Create(Statement: String; Params: ISQLParams);
-      class function New(Statement: String; Params: ISQLParams): ISQLStatement; Overload;
-      class function New(Statement: String): ISQLStatement; Overload;
-      function ParamList: ISQLParams;
-      function AsString: String;
-    End;
+  TSQLStatement = class(TInterfacedObject, ISQLStatement)
+  strict private
+    FStatement: string;
+    FParamList: ISQLParams;
+  public
+    constructor Create(Statement: string; Params: ISQLParams); overload;
+    constructor Create(Statement: string); overload;
+    class function New(Statement: string; Params: ISQLParams): ISQLStatement; overload;
+    class function New(Statement: string): ISQLStatement; overload;
+    function ParamList: ISQLParams;
+    function AsString: string;
+  end;
 
-    TSQLParam = class(TInterfacedObject, ISQLParam)
-    strict private
-      FName: String;
-      FValue: Variant;
-    public
-      constructor Create(Name: String; Value: Variant);
-      class function New(Name: String; Value: Variant): ISQLParam;
-      function Name: String;
-      function Value: Variant;
-    End;
+  TSQLParam = class(TInterfacedObject, ISQLParam)
+  strict private
+    FName: string;
+    FValue: Variant;
+  public
+    constructor Create(Name: string; Value: Variant);
+    class function New(Name: string; Value: Variant): ISQLParam;
+    function Name: string;
+    function Value: Variant;
+  end;
 
-    TSQLParams = class(TInterfacedObject, ISQLParams)
-    strict private
-      FList: TList<ISQLParam>;
-    public
-      constructor Create;
-      class function New: ISQLParams; Overload;
-      class function New(Param: ISQLParam): ISQLParams; Overload;
-      function Add(Param: ISQLParam): ISQLParams;
-      function Param(Idx: Integer): ISQLParam;
-      function AsVariantArray: TVariantArray;
-      function Count: Integer;
-    End;
+  TSQLParams = class(TInterfacedObject, ISQLParams)
+  strict private
+    FList: TList<ISQLParam>;
+  public
+    constructor Create;
+    class function New: ISQLParams; overload;
+    class function New(Param: ISQLParam): ISQLParams; overload;
+    function Add(Param: ISQLParam): ISQLParams;
+    function Param(Idx: Integer): ISQLParam;
+    function AsVariantArray: TVariantArray;
+    function Count: Integer;
+  end;
 
 implementation
 
@@ -96,155 +97,161 @@ uses
 
 { TServer }
 
-constructor TServer.Create(const HostName: String; const Port: Word; const Username, Password: String;
+constructor TServer.Create(const HostName: string; const Port: Word; const Username, Password: string;
   ServerType: TServerType);
 begin
-     FHostname   := HostName;
-     FPort       := Port;
-     FUsername   := Username;
-     FPassword   := Password;
-     FServerType := ServerType;
+  FHostname   := HostName;
+  FPort       := Port;
+  FUsername   := Username;
+  FPassword   := Password;
+  FServerType := ServerType;
 end;
 
-function TServer.Hostname: String;
+function TServer.Hostname: string;
 begin
-     Result := FHostname;
+  Result := FHostname;
 end;
 
-class function TServer.New(const HostName: String; const Port: Word; const Username, Password: String;
+class function TServer.New(const HostName: string; const Port: Word; const Username, Password: string;
   ServerType: TServerType): IServer;
 begin
-     Result := Create(HostName, Port, Username, Password, ServerType);
+  Result := Create(HostName, Port, Username, Password, ServerType);
 end;
 
-function TServer.Password: String;
+function TServer.Password: string;
 begin
-     Result := FPassword;
+  Result := FPassword;
 end;
 
 function TServer.Port: Word;
 begin
-     Result := FPort;
+  Result := FPort;
 end;
 
 function TServer.ServerType: TServerType;
 begin
-     Result := FServerType;
+  Result := FServerType;
 end;
 
-function TServer.TypeAsString: String;
+function TServer.TypeAsString: string;
 begin
-     case FServerType of
-          stMySQL      : Result := 'MySQL';
-          stMSSQL      : Result := 'SQL Server';
-          stSQLite     : Result := 'SQLite';
-          stPostgreSQL : Result := 'PostgreSQL';
-     else raise Exception.Create('Unknown Server Type');
-     end;
+  case FServerType of
+    stMySQL      : Result := 'MySQL';
+    stMSSQL      : Result := 'SQL Server';
+    stSQLite     : Result := 'SQLite';
+    stPostgreSQL : Result := 'PostgreSQL';
+  else raise Exception.Create('Unknown Server Type');
+  end;
 end;
 
-function TServer.Username: String;
+function TServer.Username: string;
 begin
-     Result := FUsername;
+  Result := FUsername;
 end;
 
 { TSQLStatement }
 
-function TSQLStatement.AsString: String;
+function TSQLStatement.AsString: string;
 begin
-     Result := FStatement;
+  Result := FStatement;
 end;
 
-constructor TSQLStatement.Create(Statement: String; Params: ISQLParams);
+constructor TSQLStatement.Create(Statement: string; Params: ISQLParams);
 begin
-     FStatement := Statement;
-     FParamList := Params;
+  FStatement := Statement;
+  FParamList := Params;
 end;
 
-class function TSQLStatement.New(Statement: String): ISQLStatement;
+constructor TSQLStatement.Create(Statement: string);
 begin
-     Result := New(Statement, nil);
+  Create(Statement, nil);
 end;
 
-class function TSQLStatement.New(Statement: String; Params: ISQLParams): ISQLStatement;
+class function TSQLStatement.New(Statement: string): ISQLStatement;
 begin
-     Result := Create(Statement, Params);
+  Result := New(Statement, nil);
+end;
+
+class function TSQLStatement.New(Statement: string; Params: ISQLParams): ISQLStatement;
+begin
+  Result := Create(Statement, Params);
 end;
 
 function TSQLStatement.ParamList: ISQLParams;
 begin
-     Result := FParamList;
+  Result := FParamList;
 end;
 
 { TSQLParam }
 
-constructor TSQLParam.Create(Name: String; Value: Variant);
+constructor TSQLParam.Create(Name: string; Value: Variant);
 begin
-     FName  := Name;
-     FValue := Value;
+  FName  := Name;
+  FValue := Value;
 end;
 
-function TSQLParam.Name: String;
+function TSQLParam.Name: string;
 begin
-     Result := FName;
+  Result := FName;
 end;
 
-class function TSQLParam.New(Name: String; Value: Variant): ISQLParam;
+class function TSQLParam.New(Name: string; Value: Variant): ISQLParam;
 begin
-     Result := Create(Name, Value);
+  Result := Create(Name, Value);
 end;
 
 function TSQLParam.Value: Variant;
 begin
-     Result := FValue;
+  Result := FValue;
 end;
 
 { TSQLParams }
 
 function TSQLParams.Add(Param: ISQLParam): ISQLParams;
 begin
-     Result := Self;
-     FList.Add(Param);
+  Result := Self;
+  FList.Add(Param);
 end;
 
 function TSQLParams.AsVariantArray: TVariantArray;
 var
-   i: Integer;
-   Param: ISQLParam;
+  i: Integer;
+  Param: ISQLParam;
 begin
-     SetLength(Result, FList.Count * 2);
-     if FList.Count>0
-        then for i := 0 to FList.Count-1 do
-                 begin
-                      Result[(i+1)*2-2] := FList[i].Name;
-                      Result[(i+1)*2-1] := FList[i].Value;
-                 end;
+  SetLength(Result, FList.Count * 2);
+  if FList.Count > 0 then begin
+    for i := 0 to FList.Count-1 do begin
+      Result[(i+1)*2-2] := FList[i].Name;
+      Result[(i+1)*2-1] := FList[i].Value;
+    end;
+  end;
 end;
 
 function TSQLParams.Count: Integer;
 begin
-     Result := FList.Count;
+  Result := FList.Count;
 end;
 
 constructor TSQLParams.Create;
 begin
-     FList := TList<ISQLParam>.Create;
+  FList := TList<ISQLParam>.Create;
 end;
 
 class function TSQLParams.New: ISQLParams;
 begin
-     Result := Create;
+  Result := Create;
 end;
 
 class function TSQLParams.New(Param: ISQLParam): ISQLParams;
 begin
-     Result := Create.Add(Param);
+  Result := Create.Add(Param);
 end;
 
 function TSQLParams.Param(Idx: Integer): ISQLParam;
 begin
-     if FList.Count>0
-        then Result := FList[Idx];
+  if FList.Count > 0 then begin
+    Result := FList[Idx];
+  end;
 end;
 
 end.
--- src/DB/Obj.SSI.DBUniImpl.pas
+++ src/DB/Obj.SSI.DBUniImpl.pas
@@ -35,25 +35,26 @@ uses
   ;
 
 type
-    TDBUniDatabase = class(TInterfacedObject, IDatabase)
-    strict private
-      FConnection: TUniConnection;
-      FProvider: TUniProvider;
-    public
-      constructor Create(Server: IServer; Database: String);
-      destructor Destroy; Override;
-      class function New(Server: IServer; Database: String): IDatabase;
-      function Connect: IDatabase;
-      function Disconnect: IDatabase;
-      function StartTransaction: IDatabase;
-      function StopTransaction(SaveChanges: Boolean = True): IDatabase;
-      function IsConnected: Boolean;
-      function Database: String;
-      function NewQuery(Statement: ISQLStatement): IDBQuery;
-      function Run(SQLStatement: ISQLStatement): IDatabase;
-    End;
+  TDBUniDatabase = class(TInterfacedObject, IDatabase)
+  strict private
+    FConnection: TUniConnection;
+    FProvider: TUniProvider;
+  public
+    constructor Create(Server: IServer; Database: string);
+    destructor Destroy; override;
+    class function New(Server: IServer; Database: string): IDatabase;
+    function Connect: IDatabase;
+    function Disconnect: IDatabase;
+    function StartTransaction: IDatabase;
+    function StopTransaction(SaveChanges: Boolean = True): IDatabase;
+    function IsConnected: Boolean;
+    function Database: string;
+    function NewQuery(const Statement: ISQLStatement): IDBQuery; overload;
+    function NewQuery(const Statement: ISQLStatement; out Destination: IDBQuery): IDatabase; overload;
+    function Run(SQLStatement: ISQLStatement): IDatabase;
+  end;
 
-    TDatabase = TDBUniDatabase;
+  TDatabase = TDBUniDatabase;
 
 implementation
 
@@ -65,48 +66,48 @@ uses
   ;
 
 type
-    TDBUniQuery = class(TInterfacedObject, IDBQuery)
-    private
-      FQuery: TUniQuery;
-      FStatement : ISQLStatement;
-      FConnection: TUniConnection;
-      procedure SetRecNo(Idx: Integer);
-      function GetRecNo: Integer;
-    public
-      constructor Create(Connection: TUniConnection; Statement: ISQLStatement);
-      destructor Destroy; Override;
-      class function New(Connection: TUniConnection; Statement: ISQLStatement): IDBQuery;
-      function Publish(DataSource: TDataSource): IDBQuery;
-      function RecordCount: Integer;
-      function FieldByName(FieldName: String): TField;
-      function ForEach(Action: TRowAction): IDBQuery;
-      function Run: IDBQuery;
-      function Insert: IDBQuery;
-      function Edit: IDBQuery;
-      function Append: IDBQuery;
-      function Post: IDBQuery;
-      function FieldValue(FieldName: String; Value: Variant): IDBQuery;
-      property RecNo: Integer read GetRecNo write SetRecNo;
-    End;
+  TDBUniQuery = class(TInterfacedObject, IDBQuery)
+  private
+    FQuery: IValue<TUniQuery>;
+    procedure SetRecNo(Idx: Integer);
+    function GetRecNo: Integer;
+  public
+    constructor Create(Connection: TUniConnection; Statement: ISQLStatement);
+    destructor Destroy; override;
+    class function New(Connection: TUniConnection; Statement: ISQLStatement): IDBQuery;
+    function Publish(DataSource: TDataSource): IDBQuery;
+    function RecordCount: Integer;
+    function FieldByName(FieldName: string): TField;
+    function ForEach(Action: TRowAction): IDBQuery;
+    function Run: IDBQuery;
+    function Insert: IDBQuery;
+    function Edit: IDBQuery;
+    function Append: IDBQuery;
+    function Post: IDBQuery;
+    function FieldValue(FieldName: string; Value: Variant): IDBQuery;
+    property RecNo: Integer
+      read GetRecNo
+      write SetRecNo;
+  end;
 
 { TDBUniQuery }
 
 function TDBUniQuery.Post: IDBQuery;
 begin
-     Result := Self;
-     FQuery.Post;
+  Result := Self;
+  FQuery.Value.Post;
 end;
 
 function TDBUniQuery.Publish(DataSource: TDataSource): IDBQuery;
 begin
-     Result := Self;
-     DataSource.DataSet := FQuery;
+  Result := Self;
+  DataSource.DataSet := FQuery.Value;
 end;
 
 function TDBUniQuery.Append: IDBQuery;
 begin
-     Result := Self;
-     FQuery.Append;
+  Result := Self;
+  FQuery.Value.Append;
 end;
 
 constructor TDBUniQuery.Create(Connection: TUniConnection; Statement: ISQLStatement);
@@ -117,53 +118,52 @@ end;
 
 destructor TDBUniQuery.Destroy;
 begin
-     if Assigned(FQuery)
-        then FQuery.Free;
-     inherited;
+  if Assigned(FQuery)
+    then FQuery.Value.Free;
+  inherited;
 end;
 
 function TDBUniQuery.Edit: IDBQuery;
 begin
-     Result := Self;
-     FQuery.Edit;
+  Result := Self;
+  FQuery.Value.Edit;
 end;
 
-function TDBUniQuery.FieldByName(FieldName: String): TField;
+function TDBUniQuery.FieldByName(FieldName: string): TField;
 begin
-     Result := FQuery.FieldByName(FieldName);
+  Result := FQuery.Value.FieldByName(FieldName);
 end;
 
-function TDBUniQuery.FieldValue(FieldName: String; Value: Variant): IDBQuery;
+function TDBUniQuery.FieldValue(FieldName: string; Value: Variant): IDBQuery;
 begin
-     Result := Self;
-     FQuery.FieldValues[FieldName] := Value;
+  Result := Self;
+  FQuery.Value.FieldValues[FieldName] := Value;
 end;
 
 function TDBUniQuery.ForEach(Action: TRowAction): IDBQuery;
 begin
-     Result := Self;
-     FQuery.First;
-     while not Eof do
-           begin
-                Action(Self);
-                FQuery.Next;
-           end;
+  Result := Self;
+  FQuery.Value.First;
+  while not Eof do begin
+    Action(Self);
+    FQuery.Value.Next;
+  end;
 end;
 
 function TDBUniQuery.GetRecNo: Integer;
 begin
-  Result := FQuery.RecNo;
+  Result := FQuery.Value.RecNo;
 end;
 
 function TDBUniQuery.Insert: IDBQuery;
 begin
-     Result := Self;
-     FQuery.Insert;
+  Result := Self;
+  FQuery.Value.Insert;
 end;
 
 class function TDBUniQuery.New(Connection: TUniConnection; Statement: ISQLStatement): IDBQuery;
 begin
-     Result := Create(Connection, Statement);
+  Result := Create(Connection, Statement);
 end;
 
 function TDBUniQuery.RecordCount: Integer;
@@ -195,84 +195,94 @@ end;
 
 procedure TDBUniQuery.SetRecNo(Idx: Integer);
 begin
-  FQuery.RecNo := Idx;
+  FQuery.Value.RecNo := Idx;
 end;
 
 { TDatabase }
 
 function TDBUniDatabase.Connect: IDatabase;
 begin
-     Result := Self;
-     FConnection.Connect;
+  Result := Self;
+  FConnection.Connect;
 end;
 
-constructor TDBUniDatabase.Create(Server: IServer; Database: String);
+constructor TDBUniDatabase.Create(Server: IServer; Database: string);
 begin
-     FConnection              := TUniConnection.Create(nil);
-     FConnection.Server       := Server.Hostname;
-     FConnection.Port         := Server.Port;
-     FConnection.Username     := Server.Username;
-     FConnection.Password     := Server.Password;
-     FConnection.Database     := Database;
-     FConnection.ProviderName := Server.TypeAsString;
-       case Server.ServerType of
-            stMySQL      : FProvider := TMySQLUniProvider.Create(nil);
-            stMSSQL      : FProvider := TSQLServerUniProvider.Create(nil);
-            stSQLite     : FProvider := TSQLiteUniProvider.Create(nil);
-            stPostgreSQL : FProvider := TPostgreSQLUniProvider.Create(nil);
-       else raise Exception.Create('Unknown Server Type');
-       end;
+  FConnection              := TUniConnection.Create(nil);
+  FConnection.Server       := Server.Hostname;
+  FConnection.Port         := Server.Port;
+  FConnection.Username     := Server.Username;
+  FConnection.Password     := Server.Password;
+  FConnection.Database     := Database;
+  FConnection.ProviderName := Server.TypeAsString;
+  case Server.ServerType of
+    stMySQL      : FProvider := TMySQLUniProvider.Create(nil);
+    stMSSQL      : FProvider := TSQLServerUniProvider.Create(nil);
+    stSQLite     : FProvider := TSQLiteUniProvider.Create(nil);
+    stPostgreSQL : FProvider := TPostgreSQLUniProvider.Create(nil);
+  else raise Exception.Create('Unknown Server Type');
+  end;
 end;
 
-function TDBUniDatabase.Database: String;
+function TDBUniDatabase.Database: string;
 begin
-     Result := FConnection.Database;
+  Result := FConnection.Database;
 end;
 
 destructor TDBUniDatabase.Destroy;
 begin
-     FProvider.Free;
-     FConnection.Free;
-     inherited;
+  FProvider.Free;
+  FConnection.Free;
+  inherited;
 end;
 
 function TDBUniDatabase.Disconnect: IDatabase;
 begin
-     Result := Self;
-     FConnection.Disconnect;
+  Result := Self;
+  FConnection.Disconnect;
 end;
 
 function TDBUniDatabase.IsConnected: Boolean;
 begin
-     Result := FConnection.Connected;
+  Result := FConnection.Connected;
 end;
 
-class function TDBUniDatabase.New(Server: IServer; Database: String): IDatabase;
+class function TDBUniDatabase.New(Server: IServer; Database: string): IDatabase;
 begin
-     Result := Create(Server, Database);
+  Result := Create(Server, Database);
 end;
 
-function TDBUniDatabase.NewQuery(Statement: ISQLStatement): IDBQuery;
+function TDBUniDatabase.NewQuery(const Statement: ISQLStatement;
+ out Destination: IDBQuery): IDatabase;
 begin
-     Result := TDBUniQuery.Create(FConnection, Statement);
+  Result := Self;
+  Destination := NewQuery(Statement);
+end;
+
+function TDBUniDatabase.NewQuery(const Statement: ISQLStatement): IDBQuery;
+begin
+  Result := TDBUniQuery.Create(FConnection, Statement);
 end;
 
 function TDBUniDatabase.Run(SQLStatement: ISQLStatement): IDatabase;
 begin
-     Result := Self;
-     FConnection.ExecSQL(SQLStatement.AsString, SQLStatement.ParamList.AsVariantArray);
+  Result := Self;
+  FConnection.ExecSQL(SQLStatement.AsString, SQLStatement.ParamList.AsVariantArray);
 end;
 
 function TDBUniDatabase.StartTransaction: IDatabase;
 begin
-     FConnection.StartTransaction;
+  FConnection.StartTransaction;
 end;
 
 function TDBUniDatabase.StopTransaction(SaveChanges: Boolean = True): IDatabase;
 begin
-     if SaveChanges
-        then FConnection.Commit
-        else FConnection.Rollback;
+  if SaveChanges then begin
+    FConnection.Commit;
+  end
+  else begin
+    FConnection.Rollback;
+  end;
 end;
 
 end.
