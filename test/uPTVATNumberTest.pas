bb8c2f4510ca77aedcb669a8e3e98c20640f7742
Nuno Picado <geral@nunopicado.com>
2017-10-25 11:29:43 +0100

    Added tests for TPTVATNumber's AsString and AsInteger methods

--- test/uPTVATNumberTest.pas
+++ test/uPTVATNumberTest.pas
@@ -17,6 +17,21 @@ type
     [TestCase('Invalid 2','123,False')]
     [TestCase('Invalid 3',',False')]
     procedure IsValidTest(const VATNumber: string; const Expected: Boolean);
+    [Test]
+    [TestCase('Valid 1','555555550,555555550,True')]
+    [TestCase('Valid 2','999999990,999999990,True')]
+    [TestCase('Valid 3','123456789,123456789,True')]
+    [TestCase('Invalid 1','111111111,222222222,False')]
+    [TestCase('Invalid 2','123,321,False')]
+    [TestCase('Invalid 3','123,abc,False')]
+    procedure AsStringTest(const VATNumberIn, VATNumberOut: string; const Expected: Boolean);
+    [Test]
+    [TestCase('Valid 1','555555550,555555550,True')]
+    [TestCase('Valid 2','999999990,999999990,True')]
+    [TestCase('Valid 3','123456789,123456789,True')]
+    [TestCase('Invalid 1','111111111,222222222,False')]
+    [TestCase('Invalid 2','123,321,False')]
+    procedure AsIntegerTest(const VATNumberIn: string; VATNumberOut: Integer; const Expected: Boolean);
   end;
 
 implementation
@@ -32,6 +47,23 @@ begin
   Assert.AreEqual(
     Expected,
     TPTVATNumber.New(VATNumber).IsValid
+  );
+end;
+
+procedure TPTVATNumberTest.AsIntegerTest(const VATNumberIn: string;
+  VATNumberOut: Integer; const Expected: Boolean);
+begin
+  Assert.AreEqual(
+    Expected,
+    VATNumberOut = TPTVATNumber.New(VATNumberIn).AsInteger
+  );
+end;
+
+procedure TPTVATNumberTest.AsStringTest(const VATNumberIn, VATNumberOut: string; const Expected: Boolean);
+begin
+  Assert.AreEqual(
+    Expected,
+    VATNumberOut = TPTVATNumber.New(VATNumberIn).AsString
   );
 end;
 
