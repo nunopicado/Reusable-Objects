db0d32f952d054e3ab729107f4efcedcb4fe6bf9
Nuno Picado <geral@nunopicado.com>
2017-11-26 01:51:01 +0000

    Added unit tests for SelectNodes

--- test/uTXPathTest.pas
+++ test/uTXPathTest.pas
@@ -28,9 +28,13 @@ type
   TXPathTest = class(TObject)
   public
     [Test]
-    [TestCase('XPath 1','/bookstore/book[2]/title,Learning XML')]
-    [TestCase('XPath 2','/bookstore/book[3]/year,2006')]
+    [TestCase('XPath SelectNode 1','/bookstore/book[2]/title,Learning XML')]
+    [TestCase('XPath SelectNode 2','/bookstore/book[3]/year,2006')]
     procedure SelectNodeTest(const NodePath, Expected: string);
+    [Test]
+    [TestCase('XPath SelectNodes 1','/bookstore,4')]
+    [TestCase('XPath SelectNodes 2','/bookstore/book[3],2')]
+    procedure SelectNodesTest(const NodePath: string; const Count: Integer);
   end;
 
 implementation
@@ -40,6 +44,16 @@ uses
   ;
 
 { TXPathTest }
+
+procedure TXPathTest.SelectNodesTest(const NodePath: string; const Count: Integer);
+begin
+  Assert.AreEqual<Integer>(
+    Count,
+    TXPath.New(XML)
+      .SelectNodes(NodePath).Nodes[0].ChildNodes
+        .Count
+  );
+end;
 
 procedure TXPathTest.SelectNodeTest(const NodePath, Expected: string);
 begin
