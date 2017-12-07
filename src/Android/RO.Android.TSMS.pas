e78d15879edd11b591716117475592dd5c68c1cd
Nuno Picado <geral@nunopicado.com>
2017-12-07 00:39:19 +0000

    Added capability to publish sent and delivered reports

--- src/Android/RO.Android.TSMS.pas
+++ src/Android/RO.Android.TSMS.pas
@@ -37,52 +37,188 @@ interface
 
 uses
     RO.ISMS
+  , RO.IValue
   , Androidapi.JNI.Telephony
   , Androidapi.JNI.JavaTypes
+  , Androidapi.JNI.App
+  , Androidapi.JNI.GraphicsContentViewText
+  , BroadcastReceiver
   ;
 
 type
   TSMS = class(TInterfacedObject, ISMS)
-  private
+  private const
+    cSentAction       = 'SMS_SENT_ACTION';
+    cDeliveredAction  = 'SMS_DELIVERED_ACTION';
+    cDestinationExtra = 'SMS_PHONE_NUMBER';
+  private var
     FMsg: JString;
+    FReport: ISMSReport;
     FSmsMgr: JSmsManager;
+    FSentIntent: IValue<JIntent>;
+    FDeliveredIntent: IValue<JIntent>;
+    FPendingSentIntent: IValue<JPendingIntent>;
+    FPendingDeliveredIntent: IValue<JPendingIntent>;
+    FBroadcast: TBroadcastReceiver;
+  private
+    procedure OnReceiveBroadCast(aContext: JContext; aIntent: JIntent; aResultCode: Integer);
+    procedure ReportSendAction(const Destination: string; const aResultCode: Integer);
+    procedure ReportDeliveryAction(const Destination: string; const aResultCode: Integer);
   public
-    constructor Create(const Msg: string);
-    class function New(const Msg: string): ISMS;
+    constructor Create(const Msg: string; const Report: ISMSReport = nil);
+    class function New(const Msg: string; const Report: ISMSReport = nil): ISMS;
     function Send(const Destination: string): ISMS;
   end;
 
 implementation
 
 uses
-    Androidapi.Helpers
+    RO.TValue
+  , RO.TIf
+  , Androidapi.Helpers
+  , FMX.Helpers.Android
+  , Androidapi.JNIBridge
+  , Androidapi.JNI.Embarcadero
   ;
 
 { TSMS }
 
-constructor TSMS.Create(const Msg: string);
+constructor TSMS.Create(const Msg: string; const Report: ISMSReport = nil);
 begin
   FMsg    := StringToJString(Msg);
+  FReport := Report;
   FSmsMgr := TJSmsManager.JavaClass.getDefault;
+
+  FSentIntent := TValue<JIntent>.New(
+    function : JIntent
+    begin
+      Result := TJIntent.Create;
+      Result.setAction(
+        StringToJString(cSentAction)
+      );
+    end
+  );
+
+  FPendingSentIntent := TValue<JPendingIntent>.New(
+    function : JPendingIntent
+    begin
+      Result := TJPendingIntent.JavaClass.getBroadcast(
+        TAndroidHelper.Context,
+        0,
+        FSentIntent.Value,
+        0
+      );
+    end
+  );
+
+  FDeliveredIntent := TValue<JIntent>.New(
+    function : JIntent
+    begin
+      Result := TJIntent.Create;
+      Result.setAction(
+        StringToJString(cDeliveredAction)
+      );
+    end
+  );
+
+  FPendingDeliveredIntent := TValue<JPendingIntent>.New(
+    function : JPendingIntent
+    begin
+      Result := TJPendingIntent.JavaClass.getBroadcast(
+        TAndroidHelper.Context,
+        0,
+        FDeliveredIntent.Value,
+        0
+      );
+    end
+  );
+
+  FBroadcast := TBroadcastReceiver.Create(OnReceiveBroadcast);
+  FBroadcast.AddActions(
+    [
+      StringToJString(cSentAction),
+      StringToJString(cDeliveredAction)
+    ]
+  );
 end;
 
-class function TSMS.New(const Msg: string): ISMS;
+class function TSMS.New(const Msg: string; const Report: ISMSReport = nil): ISMS;
 begin
-  Result := Create(Msg);
+  Result := Create(Msg, Report);
+end;
+
+procedure TSMS.OnReceiveBroadCast(aContext: JContext; aIntent: JIntent; aResultCode: integer);
+var
+  Destination: string;
+begin
+  Destination := JStringToString(
+    aIntent.getStringExtra(
+      StringToJString(cDestinationExtra)
+    )
+  );
+  if JStringToString(aIntent.getAction) = cSentAction
+    then ReportSendAction(Destination, aResultCode)
+    else ReportDeliveryAction(Destination, aResultCode);
+end;
+
+procedure TSMS.ReportDeliveryAction(const Destination: string; const aResultCode: Integer);
+begin
+  if Assigned(FReport)
+    then begin
+      FReport.Delivered(
+        Destination,
+        TIf<TSMSDeliveredResultCode>.New(
+          aResultCode = TJActivity.JavaClass.RESULT_OK,
+          drcDelivered,
+          drcCanceled
+        ).Eval
+      );
+    end;
+end;
+
+procedure TSMS.ReportSendAction(const Destination: string; const aResultCode: Integer);
+var
+  ResultCode: TSMSSentResultCode;
+begin
+  ResultCode   := srcUnknown;
+  if aResultCode = TJActivity.JavaClass.RESULT_OK
+    then ResultCode := srcSent;
+  if aResultCode = TJSmsManager.JavaClass.RESULT_ERROR_RADIO_OFF
+    then ResultCode := srcRadioOff;
+  if aResultCode = TJSmsManager.JavaClass.RESULT_ERROR_GENERIC_FAILURE
+    then ResultCode := srcGenericFailure;
+  if aResultCode = TJSmsManager.JavaClass.RESULT_ERROR_NO_SERVICE
+    then ResultCode := srcNoService;
+  if aResultCode = TJSmsManager.JavaClass.RESULT_ERROR_NULL_PDU
+    then ResultCode := srcNullPDU;
+  if Assigned(FReport)
+    then FReport.Sent(Destination, ResultCode);
 end;
 
 function TSMS.Send(const Destination: string): ISMS;
 var
   smsTo: JString;
+  PendingSentIntent: JPendingIntent;
+  PendingDeliveredIntent: JPendingIntent;
 begin
   Result := Self;
-  smsTo  := StringToJString(Destination);
+
+  FSentIntent.Value.putExtra(
+    StringToJString(cDestinationExtra),
+    StringToJString(Destination)
+  );
+  FDeliveredIntent.Value.putExtra(
+    StringToJString(cDestinationExtra),
+    StringToJString(Destination)
+  );
+
+  smsTo := StringToJString(Destination);
   FSmsMgr.sendTextMessage(
     smsTo,
     nil,
     FMsg,
-    nil,
-    nil
+    FPendingSentIntent.Value,
+    FPendingDeliveredIntent.Value
   );
 end;
 
